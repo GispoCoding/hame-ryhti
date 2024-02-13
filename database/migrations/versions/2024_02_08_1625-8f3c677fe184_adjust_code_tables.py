ed0b7300edf503947ee0ae5ecc6a9b0cbf48fea1
Riku Oja <riku.oja@kapsi.fi>
2024-02-13 16:09:40 +0200

    Fix down revision

--- database/migrations/versions/2024_02_08_1625-8f3c677fe184_adjust_code_tables.py
+++ database/migrations/versions/2024_02_08_1625-8f3c677fe184_adjust_code_tables.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "8f3c677fe184"
-down_revision: Union[str, None] = "84c37bffeca0"
+down_revision: Union[str, None] = "6ee06a6e634a"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
