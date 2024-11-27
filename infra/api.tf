16a6731011c0adba2adc364fb625eb517b6d0e63
Riku Oja <riku.oja@kapsi.fi>
2024-11-27 14:49:39 +0200

    Redeploy api at any resource change

--- infra/api.tf
+++ infra/api.tf
@@ -65,10 +65,10 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
 
   triggers = {
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.ryhti_client.id,
-      aws_api_gateway_method.ryhti_call.id,
-      aws_api_gateway_integration.lambda_integration.id,
+    redeployment = sha1(join(",",[
+      jsonencode(aws_api_gateway_resource.ryhti_client),
+      jsonencode(aws_api_gateway_method.ryhti_call),
+      jsonencode(aws_api_gateway_integration.lambda_integration),
     ]))
   }
 
